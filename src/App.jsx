ce9df69b453c676aceb0aaf7da516acc3bf8a530
Shafin <shafin.ahmed.1512023@gmail.com>
2024-01-03 00:07:10 +0600

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
+import { useState, useEffect } from "react";
 import Sector from "./components/Sector";
 import TopBar from "./components/TopBar";
 
+async function fetchData() {
+    const response = await fetch("data/data.json");
+    const data = await response.json();
+    return data;
+}
+
 const App = () => {
+    const [jsonData, setJsonData] = useState(null);
+
+    useEffect(() => {
+        fetchData().then(setJsonData);
+    }, []);
+
     return (
         <>
             <TopBar />
             <main>
                 <div className="sectors">
-                    <Sector name="Amzad" amount="800" />
-                    <Sector name="Shahana" amount="1700" />
-                    <Sector name="Sakib" amount="1000" />
-                    <Sector name="Medha" amount="800" />
-                    <Sector name="Dhara" amount="429" />
-                    <Sector name="Kotha" amount="800" />
-                    <Sector name="Arushi" amount="800" />
-                    <Sector name="Anika" amount="429" />
-                    <Sector name="Khalamoni" amount="600" />
-                    <Sector name="Sabbir" amount="498" />
-                    <Sector name="Dadi" amount="600" />
-                    <Sector name="Sefa" amount="429" />
-                    <Sector name="Toha" amount="429" />
-                    <Sector name="Tuba" amount="429" />
-                    <Sector name="Badol Uncle" amount="498" />
-                    <Sector name="Amirul Uncle" amount="498" />
-                    <Sector name="Rubel Uncle" amount="498" />
+                    {jsonData &&
+                        jsonData.sectors.map((element) => (
+                            <Sector
+                                name={element.name}
+                                amount={element.amount}
+                            />
+                        ))}
                 </div>
             </main>
         </>
